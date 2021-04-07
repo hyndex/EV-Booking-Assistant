3e5c2418a9f2d77cf574d66fa67caf1cc6529c26
Snehanshu Phukon <hello@snehanshu.com>
2021-04-08 00:32:25 +0530

    changes

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ CITIES = [
     'hojai',
     'jorhat',
     'kamrup',
-    'karbi anglong',
+    'karbianglong',
     'karimganj',
     'kokrajhar',
     'lakhimpur',
@@ -34,8 +34,43 @@ CITIES = [
     'sonitpur',
     'tinsukia',
     'udalguri',
-    ]
+    'guwahati'
+]
 # https://blog.addpipe.com/using-recorder-js-to-capture-wav-audio-in-your-html5-web-site/
+
+queue = {'baksa': 0,
+         'barpeta': 0,
+         'biswanath': 0,
+         'bongaigaon': 0,
+         'cachar': 0,
+         'charaideo': 0,
+         'chirang': 0,
+         'darrang': 0,
+         'dhemaji': 0,
+         'dhubri': 0,
+         'dibrugarh': 0,
+         'dima Hasao': 0,
+         'goalpara': 0,
+         'golaghat': 0,
+         'hailakandi': 0,
+         'hojai': 0,
+         'jorhat': 0,
+         'kamrup': 0,
+         'karbianglong': 0,
+         'karimganj': 0,
+         'kokrajhar': 0,
+         'lakhimpur': 0,
+         'majuli': 0,
+         'morigaon': 0,
+         'nagaon': 0,
+         'nalbari': 0,
+         'sivasagar': 0,
+         'sonitpur': 0,
+         'tinsukia': 0,
+         'udalguri': 0,
+         'guwahati': 0}
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     transcript = ""
@@ -54,13 +89,19 @@ def index():
             audioFile = sr.AudioFile(file)
             with audioFile as source:
                 data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
+            try:
+                transcript = recognizer.recognize_google(data, key=None)
+            except:
+                return render_template('index.html', transcript='Try again')
 
-    for req_city in (transcript.lower().split(' ')):
-        if req_city in CITIES:
-            print(req_city)
-            return render_template('index.html', transcript='charger has been booked in '+req_city)
-    return render_template('index.html', transcript='Charger not avialable in the requested city')
+        for req_city in (transcript.lower().split(' ')):
+            if req_city in CITIES:
+                print(req_city)
+                queue[req_city] = queue[req_city]+1
+                return render_template('index.html', transcript='charger has been booked in ' + req_city+' Your queue is '+str(queue[req_city]))
+        return render_template('index.html', transcript='Charger not available in the requested city')
+
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
